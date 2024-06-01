2bb3750de22a3a3ff98dce1e55ea0473c44615b6
jwkaterina <jwkaterina@gmail.com>
2024-06-01 21:18:19 +0200

    initial commit

---
+++ app/actions/fetch-photos.tsx
@@ -0,0 +1,23 @@
+'use server'
+
+const fetchPhotos = async(date: string) => {
+
+    const url = `https://api.nasa.gov/planetary/apod?api_key=${process.env.API_KEY}&date=${date}`;
+
+    try {
+        const response = await fetch(url);
+        if (!response.ok) {
+            throw new Error(response.statusText);
+        }
+        const result = await response.json();
+        console.log(result);
+
+        return result;
+        
+    } catch (err) {
+        console.error(err);
+        return err;
+    }
+};
+
+export default fetchPhotos;
